e26f68c8289815623c25c217379a0e28b6e50ca9
gorandalex <andrii.gorobets@gmail.com>
2022-11-11 18:52:50 +0200

    add class Record

---
+++ Record.py
@@ -0,0 +1,30 @@
+from Name import Name
+from Note import Notes
+from class_birthday import Birthday
+from class_num import Phone
+from class_address import Address
+
+
+class Record():
+    def __init__(self, name, address = None, phone = None, birthday = None, note = None):
+        self.name = Name(name)
+
+        if address:
+            self.address = Address(address)
+        else:
+            self.address = ''
+
+        if phone:
+            self.phones = [Phone(phone)]
+        else:
+            self.phones = []
+
+        if birthday:
+            self.birthday = Birthday(birthday)
+        else:
+            self.birthday = ''
+
+        if note:
+            self.note = Notes(note)
+        else:
+            self.note = ''
