407351ebd32701eda856a1c17812d6ad1b92ab27
YaroslavZq <svyatikua2@gmail.com>
2022-11-12 14:48:01 +0200

    add funcs add, update, delete for nums and emails

--- Record.py
+++ Record.py
@@ -33,3 +33,41 @@ class Record():
             self.note = Notes(note)
         else:
             self.note = ''
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def change_phone(self, old_phone, new_phone):
+        phone_obj = Phone(old_phone)    
+        if phone_obj in self.phones:
+            self.phones[self.phones.index(phone_obj)] = Phone(new_phone)
+            return True
+
+    def delete_phone(self, phone):
+        phone_obj = Phone(phone)    
+        if phone_obj in self.phones:
+            self.phone.remove(phone_obj)
+            return True
+    
+
+    def add_email(self, email):
+        if self.email is None:
+            self.email = Email(email)
+        else:
+            raise ValueError("""This contact {self.name} 
+            already have date of birthday: {self.birthday}
+            if you want to change tis date try "change_email | new_email"
+            """)
+
+    def change_email(self, old_email, new_email):
+        for email in self.email:
+            if email.value == old_email:
+                self.birthday.append(new_email)
+                self.birthday.remove(email)
+                return True
+
+    def delete_email(self, delete_email):
+        for email in self.email:
+            if email.value == delete_email:
+                self.birthday.remove(email)
+                return True
